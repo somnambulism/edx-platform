78ab007729c2d8f88ca8becdf8a098d50f2c45fb
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-25 15:57:44 -0400

    Refactored some code and added full elasticsearch module test suite

--- lms/urls.py
+++ lms/urls.py
@@ -129,13 +129,15 @@ urlpatterns += (
     # url(r'^reactivate/(?P<key>[^/]*)$', 'student.views.reactivation_email'),
 
     # Search
-<<<<<<< HEAD
     url(r'^search$', 'search.views.search')
+<<<<<<< HEAD
 
 =======
     url(r'^search$', 'search.views.search'),
 >>>>>>> Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction
 >>>>>>> Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction
+=======
+>>>>>>> Refactored some code and added full elasticsearch module test suite
 )
 
 # Semi-static views only used by edX, not by themes
